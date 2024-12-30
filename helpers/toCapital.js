8b3063d67954f1331b927f8778dc35e59e9995df
nato1516 <163149975+nato1516@users.noreply.github.com>
2024-12-30 14:46:50 -0600

    Delete helpers directory

--- helpers/toCapital.js
+++
@@ -1,9 +0,0 @@
-export function toCapital(str){
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-//Toma cualquier palabra y pone la primera letra en mayyuscula y las demas en minusculas 
-
-    //Ejemplos:
-    // toCapital('hola') ==> 'Hola'
-    // toCapital('mundo') ==> 'Mundo'
-    // toCapital('esto es un ejemplo') ==> 'Esto Es Un Ejemplo'
